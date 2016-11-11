d46508cd451b044504a81d1a1ae0103312bdd45d
jcraane <jcraane@gmail.com>
2016-11-11 10:40:34 +0100

    Draw shadows on all sides. todo: make colliding shadows on corners blend nice together.

--- lib/src/main/java/nl/capaxit/androidilib/ui/shadow/ShadowWrapperFrameLayout.java
+++ lib/src/main/java/nl/capaxit/androidilib/ui/shadow/ShadowWrapperFrameLayout.java
@@ -10,18 +10,23 @@ import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
+import java.util.EnumMap;
 import java.util.EnumSet;
+import java.util.Map;
 
 import nl.capaxit.androidilib.R;
 
 /**
+ * todo shadow is not present on the corners at the moment when two shadows come together. Perhaps we can solve this by drawing a radial gradient at the corners where two shadows collide.
+ *
  * Created by jamiecraane on 10/11/2016.
  */
 public class ShadowWrapperFrameLayout extends FrameLayout {
     private static final int DEFAULT_SHADOW_HEIGHT = 3; // dp;
-    private Drawable shadowDrawable;
     private int shadowHeight = -1;
     private final EnumSet<Side> sides = EnumSet.noneOf(Side.class);
+    private final Map<Side, Drawable> shadowDrawables = new EnumMap<>(Side.class);
+    private final Rect clipBounds = new Rect();
 
     public ShadowWrapperFrameLayout(final Context context) {
         this(context, null);
@@ -47,7 +52,11 @@ public class ShadowWrapperFrameLayout extends FrameLayout {
             attributes.recycle();
         }
 
-        shadowDrawable = ContextCompat.getDrawable(context, R.drawable.shadow_top);
+        shadowDrawables.put(Side.TOP, ContextCompat.getDrawable(context, R.drawable.shadow_top));
+        shadowDrawables.put(Side.LEFT, ContextCompat.getDrawable(context, R.drawable.shadow_left));
+        shadowDrawables.put(Side.BOTTOM, ContextCompat.getDrawable(context, R.drawable.shadow_bottom));
+        shadowDrawables.put(Side.RIGHT, ContextCompat.getDrawable(context, R.drawable.shadow_right));
+
         if (shadowHeight == -1) {
             final float density = context.getResources().getDisplayMetrics().density;
             shadowHeight = (int) (DEFAULT_SHADOW_HEIGHT * density + 0.5f);
@@ -59,31 +68,68 @@ public class ShadowWrapperFrameLayout extends FrameLayout {
     @Override
     protected void onDraw(final Canvas canvas) {
         super.onDraw(canvas);
-        if (sides.contains(Side.TOP)) {
-            drawTopShadow(canvas);
+        for (final Side side : sides) {
+            final Rect shadowRect = side.getShadowRect(getWidth(), getHeight(), shadowHeight);
+            final Drawable shadowDrawable = shadowDrawables.get(side);
+            shadowDrawable.setBounds(shadowRect);
+            canvas.getClipBounds(clipBounds);
+            side.inset(clipBounds, shadowHeight);
+            canvas.clipRect(clipBounds, Region.Op.REPLACE);
+            shadowDrawable.draw(canvas);
         }
     }
 
-    private void drawTopShadow(final Canvas canvas) {
-        final Rect shadowRect = new Rect(0, 0, getWidth(), shadowHeight);
-        shadowDrawable.setBounds(shadowRect);
-        Rect newRect = new Rect();
-        canvas.getClipBounds(newRect);
-        newRect.inset(0, -1 * shadowHeight);
-        canvas.clipRect(newRect, Region.Op.REPLACE);
-        canvas.save();
-        canvas.translate(0, -1 * shadowHeight);
-        shadowDrawable.draw(canvas);
-        canvas.restore();
-    }
-
     private enum Side {
-        TOP(1), LEFT(2), BOTTOM(4), RIGHT(8);
+        TOP(1) {
+            @Override
+            Rect getShadowRect(final int width, final int height, final int shadowHeight) {
+                return new Rect(0, -1 * shadowHeight, width, 0);
+            }
+
+            @Override
+            void inset(final Rect rect, int shadowHeight) {
+                rect.inset(0, -1 * shadowHeight);
+            }
+        }, LEFT(2) {
+            @Override
+            Rect getShadowRect(final int width, final int height, final int shadowHeight) {
+                return new Rect(0 - shadowHeight, 0, 0, height);
+            }
+
+            @Override
+            void inset(final Rect rect, int shadowHeight) {
+                rect.inset(-1 * shadowHeight, 0);
+            }
+        }, BOTTOM(4) {
+            @Override
+            Rect getShadowRect(final int width, final int height, final int shadowHeight) {
+                return new Rect(0, height, width, height + shadowHeight);
+            }
+
+            @Override
+            void inset(final Rect rect, int shadowHeight) {
+                rect.inset(0, -1 * shadowHeight);
+            }
+        }, RIGHT(8) {
+            @Override
+            Rect getShadowRect(final int width, final int height, final int shadowHeight) {
+                return new Rect(width, 0, width + shadowHeight, height);
+            }
+
+            @Override
+            void inset(final Rect rect, int shadowHeight) {
+                rect.inset(-1 * shadowHeight, 0);
+            }
+        };
 
         private final int value;
 
         Side(final int value) {
             this.value = value;
         }
+
+        abstract Rect getShadowRect(int width, int height, int shadowHeight);
+
+        abstract void inset(Rect rect, int shadowHeight);
     }
 }
